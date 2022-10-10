5d8a4557563d684ce5a1a871ba78769be66c7b81
Juan Manaure <juan.manaure@gmail.com>
2022-10-09 23:18:47 -0300

    teste

---
+++ recipes/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luiz Otávio',
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luiz Otávio',
+    })
